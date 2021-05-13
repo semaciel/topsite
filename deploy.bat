d23f97bae646d0669733ea5a855bc95501a5cbde
semaciel <sam.e.maciel@gmail.com>
2021-05-13 16:57:03 -0500

    batver

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+title Deploy Script
+@echo Starting...
+# abort on errors
+set -e
+# build
+npm run build
+# navigate into the build output directory
+cd dist
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:username/topsite.git main:gh-pages
+cd -
