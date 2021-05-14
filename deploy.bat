e5acaddb35b074136ef38dce1aa16e7557ce9997
semaciel <sam.e.maciel@gmail.com>
2021-05-14 01:06:12 -0500

    whaa

--- deploy.bat
+++ deploy.bat
@@ -9,14 +9,14 @@ echo %cd%
 cmd /c npm run build
 ::@echo --navigate into the build output directory
 echo %cd%
-cd\dist
+cd dist
 echo %cd%
 git init
 git add -A
 @echo All complete
-git commit -m 'deploy %date%'
+git commit -m 'deploy'
 
 @echo deploy message
 git push -f git@github.com:semaciel/topsite.git main:gh-pages
-cd -
+cd ..
 @echo Logged time = %time% %date%
