e94943e763e3c6a1bd4b71de1e70a3cc60e0983d
semaciel <sam.e.maciel@gmail.com>
2021-05-14 10:49:41 -0500

    yay

--- deploy.bat
+++ deploy.bat
@@ -1,21 +1,20 @@
 @echo off
 title Deploy Script
 @echo --Starting...
+echo %cd%
 @echo --Del nodemodules
 del node_modules /q
-npm i
+cmd /c npm i
 echo %cd%
 @echo --npm run build
 cmd /c npm run build
-::@echo --navigate into the build output directory
-echo %cd%
+@echo --navigate into the build output directory
 cd dist
 echo %cd%
 git init
 git add -A
 @echo All complete
 git commit -m 'deploy'
-
 @echo deploy message
 git push -f git@github.com:semaciel/topsite.git main:gh-pages
 cd ..
